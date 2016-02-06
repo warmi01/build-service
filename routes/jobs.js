9608f5dc079bb1ee7a95ff1777412a5fd9a5fbab
Anhtram Nguyen <nguan01@ca.com>
2016-02-06 01:43:15 -0500

    Implement job create

--- routes/jobs.js
+++ routes/jobs.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var router = express.Router();
 var querystring = require('querystring');
+var fs = require('fs');
+var path = require('path');
+var parser = require('xml2json');
 var jenkins = require('jenkins')('http://localhost:8080');
 
 // GET list of jobs
@@ -18,16 +21,26 @@ router.get('/', function(req, res, next) {
 
 //POST job (create)
 router.post('/:name', function(req, res, next) {
-	var xml = "";
+    var sampleXml = path.join(__dirname, 'config.xml');
+	var data = fs.readFileSync(sampleXml);
 
-	jenkins.job.create(req.params.name, xml, function(err) {
-    	    if (err) next(err);
-	    //console.log('job:' + req.params.name, data);
-	    //res.setHeader("Content-Type", "application/json;charset=UTF-8");
-	    //res.send(data);
-	  });
+	var jsonOutput = parser.toJson(data, {reversible: true, sanitize: false, coerce: true, object: true});
+	
+	//replace with values from request
+	jsonOutput["flow-definition"].definition.scm.userRemoteConfigs["hudson.plugins.git.UserRemoteConfig"].url.$t = req.body.giturl;
+    jsonOutput["flow-definition"].definition.scriptPath.$t = req.body.scriptpath;
+	
+	var xmlOutput = parser.toXml(jsonOutput);
 
-	});
+	jenkins.job.create(req.params.name, xmlOutput, function(err, data) {
+	    if (err) throw err;
+
+	    //console.log('create job data:', data);
+	    res.setHeader("Content-Type", "application/xml");
+	    res.send(data);
+   	});
+});
+
 
 //POST job build (run)
 router.post('/:name/builds', function(req, res, next) {
