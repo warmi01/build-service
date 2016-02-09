a6c8fabd7fab4861425f01c666ef33f9e426fbf4
Mickey Ward <mickey.ward@ca.com>
2016-02-09 16:36:59 -0500

    Wrap jobs data

--- routes/jobs.js
+++ routes/jobs.js
@@ -4,21 +4,63 @@ var querystring = require('querystring');
 var fs = require('fs');
 var path = require('path');
 var parser = require('xml2json');
-//var jenkins = require('jenkins')('http://localhost:8080');
-var jenkins = require('jenkins')('http://192.168.99.100:8080');
+var host = "http://localhost:8080";
+var jenkins = require('jenkins')(host);
+//var jenkins = require('jenkins')('http://192.168.99.100:8080');
+
+var CI_NEW 	   = 'CI_New',
+	CI_RUNNING = 'CI_Running',
+	CI_PASSING = 'CI_Passing',
+	CI_FAILING = 'CI_Failing';
+
+var NOTBUILT   	  = 'notbuilt',
+	NOTBUILT_ANIM = 'notbuilt_anim',
+	BLUE	   	  = 'blue',
+	RED	   		  = 'red',
+	ABORTED	   	  = 'aborted';
 
 // GET list of jobs
 router.get('/', function(req, res, next) {
+	
+  var json = { jobs: [], 'jenkins-jobs': [] };
 
   jenkins.job.list(function(err, data) {
     if (err) next(err);
     
     console.log('job list', data);
-    res.setHeader("Content-Type", "application/json;charset=UTF-8");
-    res.send(data);
+    
+    // Build json result
+    data.forEach(function(item) {
+    	var job = {};
+    	job.name = item.name;
+    	job.url = host + '/jobs/' + item.name;
+    	job['build-status'] = buildStatus(item.color);
+    	json.jobs.push(job);
+    });
+    
+    json['jenkins-jobs'] = data;
+    
+    res.send(json);
   });
 
 });
+
+function buildStatus(color) {
+	switch (color) {
+	case NOTBUILT:
+		return CI_NEW;
+	case NOTBUILT_ANIM:
+		return CI_RUNNING;
+	case BLUE:
+		return CI_PASSING;
+	case RED:
+		return CI_FAILING;
+	case ABORTED:
+		return CI_FAILING;
+	default:
+		return CI_NEW;
+	}
+}
 
 //POST job (create)
 router.post('/:name', function(req, res, next) {
@@ -49,7 +91,6 @@ router.post('/:name', function(req, res, next) {
 	jenkins.job.create(req.params.name, xmlOutput, function(err, data) {
 	    if (err) throw err;
 
-	    //res.setHeader("Content-Type", "application/xml");
 	    //res.send(data);
 
 	    //start a job build
@@ -71,9 +112,6 @@ router.post('/:name/builds', function(req, res, next) {
 
 	jenkins.job.build(req.params.name, function(err) {
     	    if (err) next(err);
-	    //console.log('job:' + req.params.name, data);
-	    //res.setHeader("Content-Type", "application/json;charset=UTF-8");
-	    //res.send(data);
 	  });
 
 	});
@@ -83,9 +121,6 @@ router.delete('/:name', function(req, res, next) {
 
 	  jenkins.job.destroy(req.params.name, function(err) {
 	    if (err) next(err);
-	    //console.log('job:' + req.params.name, data);
-	    //res.setHeader("Content-Type", "application/json;charset=UTF-8");
-	    //res.send(data);
 	  });
 
 	});
@@ -97,7 +132,6 @@ router.get('/:name', function(req, res, next) {
     	    if (err) next(err);
 
 	    console.log('job:' + req.params.name, data);
-	    res.setHeader("Content-Type", "application/json;charset=UTF-8");
 	    res.send(data);
 	  });
 
@@ -114,19 +148,5 @@ router.get('/:name/builds/:number', function(req, res, next) {
 	  });
 
 	});
-
-/*
-router.get('/config/:name', function(req, res, next) {
-
-  jenkins.job.config(req.params.name, function(err, data) {
-    if (err) next(err);
-
-    console.log('xml:' + req.params.name, data);
-    res.setHeader('Content-Type', 'application/xml');
-    res.send(data);
-  });
-
-});
-*/
 
 module.exports = router;
