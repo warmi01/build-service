946ae77b00d9a40bf51ed0d4736f8e0ab2034687
Mickey Ward <mickey.ward@ca.com>
2016-02-10 17:08:59 -0500

    More create job work

--- routes/jobs.js
+++ routes/jobs.js
@@ -3,21 +3,28 @@ var router = express.Router();
 var fs = require('fs');
 var path = require('path');
 var parser = require('xml2json');
+
 var jenkinsPort = process.env.JENKINS_SERVICE_PORT || 8080;
 var jenkinsHost = process.env.JENKINS_SERVICE_HOST || '127.0.0.1';
 var host = 'http://' + jenkinsHost + ':' + jenkinsPort;
 var jenkins = require('jenkins')(host);
 
-var CI_NEW 	   = 'CI_New',
-	CI_RUNNING = 'CI_Running',
-	CI_PASSING = 'CI_Passing',
-	CI_FAILING = 'CI_Failing';
+var CI_NEW 	   	= 'CI_New',
+	CI_RUNNING 	= 'CI_Running',
+	CI_ABORTING = 'CI_Aborting',
+	CI_PASSING 	= 'CI_Passing',
+	CI_FAILING 	= 'CI_Failing';
 
 var NOTBUILT   	  = 'notbuilt',
 	NOTBUILT_ANIM = 'notbuilt_anim',
 	BLUE	   	  = 'blue',
 	RED	   		  = 'red',
 	ABORTED	   	  = 'aborted';
+
+// get protocol+host+port
+function getHost(req) {
+	return req.protocol + '://' + req.get('Host');
+}
 
 // GET list of jobs
 router.get('/', function(req, res, next) {
@@ -33,7 +40,7 @@ router.get('/', function(req, res, next) {
     data.forEach(function(item) {
     	var job = {};
     	job.name = item.name;
-    	job.url = host + '/jobs/' + item.name;
+    	job.url = getHost(req) + '/jobs/' + item.name;
     	job['build-status'] = buildStatus(item.color);
     	json.jobs.push(job);
     });
@@ -56,13 +63,13 @@ function buildStatus(color) {
 	case RED:
 		return CI_FAILING;
 	case ABORTED:
-		return CI_FAILING;
+		return CI_ABORTING;
 	default:
 		return CI_NEW;
 	}
 }
 
-//POST job (create)
+// POST job (create)
 router.post('/:name', function(req, res, next) {
 	
 	if (req.body.giturl == null || req.body.scriptpath == null) {
@@ -87,17 +94,21 @@ router.post('/:name', function(req, res, next) {
 		jenkins.job.create(req.params.name, xmlOutput, function(err, data) {
 			if (err) return next(err);
 	
-		    // run job
-		    runJob(req.params.name, function(err) {
-			    if (err) next(err);    	
+		    // return job to caller
+		    getJob(req.params.name, function(err, data) {
+				if (err) return next(err);
 
-			    // return job to caller
-			    getJob(req.params.name, function(err, data) {
-					if (err) return next(err);
+				// run job
+			    runJob(req.params.name, function(err) {
+				    
+				    var json = {job: {url: null}, build: {url: null}};
+				    
+				    json.job.url = getHost(req)  + '/jobs/' + req.params.name;
+			    	json.build.url = (err ? null : getHost(req)  + '/jobs/' + req.params.name + '/builds/' + data['jenkins-job'].nextBuildNumber);				    	
 
-				    res.send(data);
-				});
-		    });
+					res.send(json);
+			    });
+			}); 
 	   	});
     });
  });
